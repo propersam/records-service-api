044cf5f79cf4c5a54ca7cdd53d66c71ed76f7b20
Samuel Ameh <jcnwobodo@suretrade.ng>
2019-01-21 05:45:01 +0100

    fixed gitlab-ci.yml file5

--- sureedurecords/settings.py
+++ sureedurecords/settings.py
@@ -26,7 +26,8 @@ SITE_ROOT = root()
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'aaekb)r6%#*bz@$zfm8-4#4s-9=iiho3)af!1+9pl=+&o4(8px'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')  # FALSE by default if not in os.environ
