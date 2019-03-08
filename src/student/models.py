3e7b4689c5f2a09ff71e177f0d09772056165402
propersam <propersam2012@gmail.com>
2019-03-08 20:27:58 +0100

    fix parent error

--- src/student/models.py
+++ src/student/models.py
@@ -32,7 +32,7 @@ class Parent(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.full_name
+        return self.user_id
 
     class Meta:
         ordering = ('-updated_at',)
