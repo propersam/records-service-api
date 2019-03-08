871d385950ee7b2183f45996146419385c6ce460
propersam <propersam2012@gmail.com>
2019-03-08 20:17:23 +0100

    add bugsnag to middlewares

--- src/sureedurecords/settings/common.py
+++ src/sureedurecords/settings/common.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'bugsnag.django.middleware.BugsnagMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
